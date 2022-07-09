7508e64f90ecc4fc902e92351621a9ade674416b
Matthew Hayter <matthewhayter@gmail.com>
2022-07-09 11:35:00 +0100

    Pare down unneeded html-related files.

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-import cookieParser from 'cookie-parser';
-import morgan from 'morgan';
+// import cookieParser from 'cookie-parser';
+// import morgan from 'morgan';
 import path from 'path';
-import helmet from 'helmet';
+// import helmet from 'helmet';
 
 import express, { NextFunction, Request, Response } from 'express';
-import StatusCodes from 'http-status-codes';
+// import StatusCodes from 'http-status-codes';
 import 'express-async-errors';
 
-import apiRouter from './routes/api';
-import logger from 'jet-logger';
-import { CustomError } from '@shared/errors';
+// import apiRouter from './routes/api';
+// import logger from 'jet-logger';
+// import { CustomError } from '@shared/errors';
 
 
 // Constants
@@ -23,17 +23,17 @@ const app = express();
 // Common middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // Show routes called in console during development
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-}
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(morgan('dev'));
+// }
 
 // Security (helmet recommended in express docs)
-if (process.env.NODE_ENV === 'production') {
-    app.use(helmet());
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(helmet());
+// }
 
 
 /***********************************************************************************
@@ -41,16 +41,16 @@ if (process.env.NODE_ENV === 'production') {
  **********************************************************************************/
 
 // Add api router
-app.use('/api', apiRouter);
+// app.use('/api', apiRouter);
 
 // Error handling
-app.use((err: Error | CustomError, _: Request, res: Response, __: NextFunction) => {
-    logger.err(err, true);
-    const status = (err instanceof CustomError ? err.HttpStatus : StatusCodes.BAD_REQUEST);
-    return res.status(status).json({
-        error: err.message,
-    });
-});
+// app.use((err: Error | CustomError, _: Request, res: Response, __: NextFunction) => {
+//     logger.err(err, true);
+//     const status = (err instanceof CustomError ? err.HttpStatus : StatusCodes.BAD_REQUEST);
+//     return res.status(status).json({
+//         error: err.message,
+//     });
+// });
 
 
 /***********************************************************************************
@@ -58,18 +58,23 @@ app.use((err: Error | CustomError, _: Request, res: Response, __: NextFunction) 
  **********************************************************************************/
 
 // Set views dir
-const viewsDir = path.join(__dirname, 'views');
-app.set('views', viewsDir);
+// const viewsDir = path.join(__dirname, 'views');
+// app.set('views', viewsDir);
 
 // Set static dir
-const staticDir = path.join(__dirname, 'public');
-app.use(express.static(staticDir));
+// const staticDir = path.join(__dirname, 'public');
+// app.use(express.static(staticDir));
 
 // Serve index.html file
-app.get('*', (_: Request, res: Response) => {
-    res.sendFile('index.html', {root: viewsDir});
-});
+// app.get('*', (_: Request, res: Response) => {
+//     // res.sendFile('index.html', {root: viewsDir});
+//     res.send({});
+// });
 
+app.post("/claim-free-share", (_: Request, res: Response) => {
+    // res.sendFile('index.html', {root: viewsDir});
+    res.send("Yep that works.");
+});
 
 
 // Export here and start in a diff file (for testing).
